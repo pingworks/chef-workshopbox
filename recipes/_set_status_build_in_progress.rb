76285136e37e47819357a56042c6cd4a37615528
Alexander Birk <birk@pingworks.de>
2015-09-28 21:49:22 +0200

    create dirs first

--- recipes/_set_status_build_in_progress.rb
+++ recipes/_set_status_build_in_progress.rb
@@ -6,6 +6,23 @@
 # Licensed under the Apache License, Version 2.0
 #
 
+
+directory '/var/run/workshopbox' do
+  owner 'root'
+  group 'root'
+  mode 00755
+  recursive true
+  action :create
+end
+
+directory '/etc/workshopbox' do
+  owner 'root'
+  group 'root'
+  mode 00755
+  recursive true
+  action :create
+end
+
 template '/var/run/workshopbox/status' do
   variables({
     :status => 'build_in_progress:' + run_context.cookbook_collection['ws-workshopbox'].metadata.version
