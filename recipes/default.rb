ee9da13171fd48d5c82188bfe57313f2e1a585a8
Alexander Birk <birk@pingworks.de>
2015-09-29 00:25:23 +0200

    overwrite id_rsa

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,9 @@
 # Licensed under the Apache License, Version 2.0
 #
 
+# make sure that no installation key is still active
+cookbook_file '/tmp/id_rsa'
+
 include_recipe 'ws-workshopbox::_set_status_build_in_progress'
 
 include_recipe 'ohai'
