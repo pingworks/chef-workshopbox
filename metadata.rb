41c1f0f00fe304476c66df3609d7ee4b6f044f7a
Alexander Birk <birk@pingworks.de>
2015-09-29 01:16:43 +0200

    1st fully automated build

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'birk@pingworks.de'
 license          'Apache 2.0'
 description      'Installs/Configures ws-wbox-base'
 long_description 'Installs/Configures ws-wbox-base'
-version          '0.2.7'
+version          '0.3.0'
 
 depends 'apt'
 depends 'ohai'
