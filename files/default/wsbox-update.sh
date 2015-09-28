6e03dcaab376c2cd3cc9ee7fe7e41313ed42ef9c
Alexander Birk <birk@pingworks.de>
2015-09-29 01:46:39 +0200

    simple wsbox updater

--- files/default/wsbox-update.sh
+++ files/default/wsbox-update.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+TS=$(date +%Y-%m-%d_%H%M%S)
+
+mkdir /var/tmp/$TS
+
+cat <<EOF > /var/tmp/$TS/node.json
+{
+  "run_list": "recipe[ws-workshopbox::default]"
+}
+EOF
+
+cat <<EOF > /var/tmp/$TS/solo.rb
+cookbook_path [ "/opt/workshopbox/lib/cookbooks" ]
+log_level :info
+log_location "/var/tmp/${TS}/log"
+verify_api_cert true
+EOF
+
+chef-solo -j /var/tmp/${TS}/node.json -c /var/tmp/${TS}/solo.rb
