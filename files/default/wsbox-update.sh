016ad7c8676c636b799d30e1be6a5233c454eaff
Alexander Birk <birk@pingworks.de>
2015-09-29 09:03:46 +0200

    wsbox-update now with version check

--- files/default/wsbox-update.sh
+++ files/default/wsbox-update.sh
@@ -1,20 +1,53 @@
 #!/bin/bash
+OLDDIR=$(pwd)
 
-TS=$(date +%Y-%m-%d_%H%M%S)
-
-mkdir /var/tmp/$TS
-
-cat <<EOF > /var/tmp/$TS/node.json
-{
-  "run_list": "recipe[ws-workshopbox::default]"
+function vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
 }
-EOF
 
-cat <<EOF > /var/tmp/$TS/solo.rb
-cookbook_path [ "/opt/workshopbox/lib/cookbooks" ]
-log_level :info
-log_location "/var/tmp/${TS}/log"
-verify_api_cert true
-EOF
+CUR_VERSION=$(</etc/workshopbox/version)
 
-chef-solo -j /var/tmp/${TS}/node.json -c /var/tmp/${TS}/solo.rb
+echo 'Checking cookbook ws-workshopbox for newer version...'
+cd /opt/workshopbox/lib/cookbooks/chef-ws-workshopbox
+git pull
+
+AVAIL_VERSION=$(cat metadata.rb | grep version | sed -e 's;^[^0-9]*\([0-9.]*\)[^0-9.]*$;\1;')
+
+echo "Current Version:   ${CUR_VERSION}"
+echo "Available Version: ${AVAIL_VERSION}"
+vercomp "${CUR_VERSION}" "${AVAIL_VERSION}"
+if [ $? -eq 2 ];then
+  echo "Updating Workshopbox to Version ${AVAIL_VERSION}..."
+  mofa provision . -t localhost
+else
+  echo "Workshopbox is already up to date."
+fi
+
+cd $OLDDIR
