47a821a058bdc46052f1c4056ed2a5927ff99faa
Alexander Birk <birk@pingworks.de>
2015-10-12 09:43:54 +0200

    test kitchen local yml for dry run testing

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,6 @@
+# Dry run (for faster roundtrips while build infrastruture testing)
+default['ws-workshopbox']['dryrun'] = false
+
 # User Setup Prerequisit:
 # a secret-service client repo (just a simple folderstructure) exists at:
 default['ws-workshopbox']['secret-service']['client']['repo'] = '/var/lib/secret-service'
@@ -5,23 +8,23 @@ default['ws-workshopbox']['secret-service']['client']['user'] = 'root'
 
 # Various Download Locations (to be mirrored in secluded environments like Conference Hotels)
 default['ws-workshopbox']['mirror']['apt'] = 'de.archive.ubuntu.com'
-#default['ws-workshopbox']['mirror']['apt'] = 'apt-mirror-ubuntu1404.la.pingworks.net'
+# default['ws-workshopbox']['mirror']['apt'] = 'apt-mirror-ubuntu1404.la.pingworks.net'
 
 default['ws-workshopbox']['mirror']['vbox'] = 'download.virtualbox.org'
 
 default['ws-workshopbox']['mirror']['chef'] = 'opscode-omnibus-packages.s3.amazonaws.com'
-#default['ws-workshopbox']['mirror']['chef'] = 'apt-mirror-ubuntu1404.la.pingworks.net'
+# default['ws-workshopbox']['mirror']['chef'] = 'apt-mirror-ubuntu1404.la.pingworks.net'
 
-#default['ws-workshopbox']['download']['chefdk']['url'] = "http://#{node['ws-workshopbox']['mirror']['chef']}/ubuntu/12.04/x86_64/chefdk_0.7.0-1_amd64.deb"
+# default['ws-workshopbox']['download']['chefdk']['url'] = "http://#{node['ws-workshopbox']['mirror']['chef']}/ubuntu/12.04/x86_64/chefdk_0.7.0-1_amd64.deb"
 default['ws-workshopbox']['download']['chefdk']['sha256'] = '58b2e95768427f479b2114e02c924af1c51ed6b98fce829b439cc90692c3ca64'
 default['ws-workshopbox']['download']['chefdk']['url'] = "https://#{node['ws-workshopbox']['mirror']['chef']}/ubuntu/12.04/x86_64/chefdk_0.7.0-1_amd64.deb"
 
 default['ws-workshopbox']['kitchen-docker']['baseimg'] = 'pingworks/docker-ws-baseimg:0.2'
 default['ws-workshopbox']['kitchen-docker']['testcookbook']['name'] = 'chef-ws-base'
 default['ws-workshopbox']['kitchen-docker']['testcookbook']['url'] = 'https://github.com/pingworks/chef-ws-base.git'
-#default['ws-workshopbox']['kitchen-docker']['testcookbook'] = 'git@github.com:pingworks/chef-ws-kitchen-docker-test.git'
+# default['ws-workshopbox']['kitchen-docker']['testcookbook'] = 'git@github.com:pingworks/chef-ws-kitchen-docker-test.git'
 
-#default['ws-workshopbox']['domain'] = 'la.pingworks.net'
+# default['ws-workshopbox']['domain'] = 'la.pingworks.net'
 default['ws-workshopbox']['domain'] = 'ws.pingworks.net'
 default['ws-workshopbox']['cname'] = 'workshopbox'
 
