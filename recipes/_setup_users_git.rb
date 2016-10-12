a8a76b3df7a584578192f14b704f1fc8c573412d
Alexander Birk <birk@pingworks.de>
2016-10-12 20:02:49 +0200

    done

--- recipes/_setup_users_git.rb
+++ recipes/_setup_users_git.rb
@@ -10,15 +10,14 @@
 Dir.foreach(node['workshopbox']['secret_service']['client']['repo'] + '/user') do |username|
   next if username == '.' || username == '..'
 
-  bash 'setting up git for user ' + username do
-    user username
-    group username
-    environment ({ 'HOME' => "/home/#{username}", 'USER' => username })
-    code <<-EOC
-      git config --global user.email "$(cat #{node['workshopbox']['secret_service']['client']['repo']}/user/#{username}/email)"
-      git config --global user.name "$(cat #{node['workshopbox']['secret_service']['client']['repo']}/user/#{username}/firstname) $(cat #{node['workshopbox']['secret_service']['client']['repo']}/user/#{username}/lastname)"
-    EOC
-  end
+  # bash 'setting up git for user ' + username do
+  #   user username
+  #   environment ({ 'HOME' => "/home/#{username}", 'USER' => username })
+  #   code <<-EOC
+  #     git config --global user.email "$(cat #{node['workshopbox']['secret_service']['client']['repo']}/user/#{username}/email)"
+  #     git config --global user.name "$(cat #{node['workshopbox']['secret_service']['client']['repo']}/user/#{username}/firstname) $(cat #{node['workshopbox']['secret_service']['client']['repo']}/user/#{username}/lastname)"
+  #   EOC
+  # end
 
   cookbook_file "/home/#{username}/.ssh/config" do
     owner username
@@ -26,20 +25,16 @@ Dir.foreach(node['workshopbox']['secret_service']['client']['repo'] + '/user') d
     mode 0600
   end
 
-  firstname = File.read("#{node['workshopbox']['secret_service']['client']['repo']}/user/#{username}/firstname")
-  firstname.chomp!
-  lastname = File.read("#{node['workshopbox']['secret_service']['client']['repo']}/user/#{username}/lastname")
-  lastname.chomp!
-  email = File.read("#{node['workshopbox']['secret_service']['client']['repo']}/user/#{username}/email")
-  email.chomp!
-  fullname = firstname + ' ' + lastname
+  firstname = File.read("#{node['workshopbox']['secret_service']['client']['repo']}/user/#{username}/firstname").chomp
+  lastname = File.read("#{node['workshopbox']['secret_service']['client']['repo']}/user/#{username}/lastname").chomp
+  email = File.read("#{node['workshopbox']['secret_service']['client']['repo']}/user/#{username}/email").chomp
 
   template "/home/#{username}/.gitconfig" do
     owner username
     group username
     mode 0644
     variables(
-      fullname: fullname,
+      fullname: "#{firstname} #{lastname}",
       email: email
     )
   end
